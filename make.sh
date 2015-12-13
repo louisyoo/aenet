2d72654e2e0a505be27dcd3a7b544178aa285067
lchb369 <lchb@qq.com>
2015-12-13 08:07:57 +0800

    xx

---
+++ make.sh
@@ -0,0 +1,2 @@
+
+gcc config.h zmalloc.h ae.h ae.c ae_epoll.c zmalloc.c test.c -o test
--- test.c
+++ test.c
@@ -1,1 +1,24 @@
-ffff
+
+
+#include <stdio.h>
+#include "zmalloc.h"
+
+
+typedef struct user_t
+{
+  int no;
+  char* name;
+
+}user;
+int main()
+{
+  user* u=(user*)zmalloc( sizeof(user));
+
+  u->no = 1000;
+  u->name = "xxxxxx";
+
+  printf( "user no = %d,name=%s \n" ,u->no , u->name );
+  zfree( u );
+
+  return 0;
+}
