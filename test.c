c12efb95aaf768188fd06a47a5c41aa3995e3bd8
bona <bona@infinite.inc>
2015-12-22 17:40:05 +0800

    xx

--- test.c
+++ test.c
@@ -10,7 +10,7 @@
 #include <errno.h>
 #include <sys/socket.h>
 
-#define MAXFD 5
+#define MAXFD 1024
 
 void acceptCommonHandler( aeEventLoop *el , int fd, int flags);
 userClient *newClient( aeEventLoop *el , int fd);
@@ -27,7 +27,7 @@ typedef struct UserClient
 
 void loop_init(struct aeEventLoop *l) 
 {
-        puts("I'm loop_init!!! \n");
+    puts("I'm loop_init!!! \n");
 }
 
 void acceptTcpHandler(aeEventLoop *el, int fd, void *privdata, int mask) {
@@ -87,14 +87,13 @@ userClient* newClient( aeEventLoop *el , int fd)
 
 void free_client( aeEventLoop *el , userClient* c  )
 {
-       if (c->fd != -1)
-       {
-        aeDeleteFileEvent( el,c->fd,AE_READABLE);
-        aeDeleteFileEvent( el,c->fd,AE_WRITABLE);
-        close(c->fd);
-      } 
-      zfree(c); 
-
+   if (c->fd != -1)
+   {
+		aeDeleteFileEvent( el,c->fd,AE_READABLE);
+		aeDeleteFileEvent( el,c->fd,AE_WRITABLE);
+		close(c->fd);
+   } 
+   zfree(c); 
 }
 
 void on_read( userClient *c , int len )
@@ -151,35 +150,38 @@ void fin_cb(struct aeEventLoop *l,void *data)
         puts("call the unknow final function \n");
 }
 
+//监听信号事件,中断退出时要相应处理保存等操作
+
+
+
 int main(int argc,char *argv[])
 {
-        aeEventLoop *l; 
-        char *msg = "Here std say:";
-        char *user_data = malloc(50*sizeof(char));
-        if(! user_data)
-                assert( ("user_data malloc error",user_data) );
-        memset(user_data,'\0',50);
-        memcpy(user_data,msg,sizeof(msg));
-        
-        int sockfd[2];
-        int sock_count = 0;            
+	aeEventLoop *l; 
+	char *msg = "Here std say:";
+	char *user_data = malloc(50*sizeof(char));
+	if(! user_data)
+			assert( ("user_data malloc error",user_data) );
+	memset(user_data,'\0',50);
+	memcpy(user_data,msg,sizeof(msg));
+	
+	int sockfd[2];
+	int sock_count = 0;          
+	listenToPort( "0.0.0.0", 3002 , sockfd , &sock_count );
+	printf( "listen count %d,listen fd %d \n",sock_count,sockfd[0] );
 
-        listenToPort( "0.0.0.0", 3002 , sockfd , &sock_count );
-        printf( "listen count %d,listen fd %d \n",sock_count,sockfd[0] );
+	l = aeCreateEventLoop( 1024 );
+	aeSetBeforeSleepProc(l,loop_init);
+	int res;
 
-        l = aeCreateEventLoop( 1024 );
-        aeSetBeforeSleepProc(l,loop_init);
-        int res;
+	res = aeCreateFileEvent(l,sockfd[0],AE_READABLE,acceptTcpHandler,user_data);
+	printf("create file event is ok? [%d]\n",res==0 );
 
-        res = aeCreateFileEvent(l,sockfd[0],AE_READABLE,acceptTcpHandler,user_data);
-        printf("create file event is ok? [%d]\n",res==0 );
+	res = aeCreateTimeEvent(l,5*1000,time_cb,NULL,fin_cb);
+	printf("create time event is ok? [%d]\n",!res);
 
-        res = aeCreateTimeEvent(l,5*1000,time_cb,NULL,fin_cb);
-        printf("create time event is ok? [%d]\n",!res);
+	aeMain(l);
 
-        aeMain(l);
-
-        aeDeleteEventLoop(l);
-        puts("Everything is ok !!!\n");
+	aeDeleteEventLoop(l);
+	puts("Everything is ok !!!\n");
 	return 0;
 }
